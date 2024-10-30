a7041b7ae9b73334ab97ecb20023446a4d9d300d
Donato Costa <donatocosta71@gmail.com>
2024-10-30 20:48:02 -0300

    finalizo los formularios

--- apps/informe/urls.py
+++ apps/informe/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('ventas/filtradas', views.informe_ventas_filtradas, name='informe_ventas_filtradas'),
     path('ventas', views.informe_ventas, name='informe_ventas'),
     path('prod_faltantes', views.informe_productos_faltantes, name='prod_faltantes'),
-    path('matprima_faltante', views.informe_matprima_faltante, name='matprima_faltante')
+    path('matprima_faltante', views.informe_matprima_faltante, name='matprima_faltante'),
+    path('productos_mas_vendidos', views.informe_productos_mas_vendidos, name='productos_mas_vendidos'),
+    path('vendedores_mas_ventas',views.informe_empleados_mas_ventas, name='vendedores_mas_ventas' )
 ]
--- apps/informe/views.py
+++ apps/informe/views.py
@@ -1,11 +1,15 @@
 from xml.dom.minidom import Document
 
-from django.db.models import F
+from django.db.models import F, Count
 from django.shortcuts import render
-
 from apps.pedido.models import MateriaPrima
 from apps.producto.models import Producto
 from apps.venta.models import Venta
+from django.shortcuts import render
+from django.db.models import Sum
+from apps.producto.models import Producto
+from apps.venta.models import Venta, Item
+from datetime import datetime
 
 # Create your views here.
 def informe_ventas(request):
@@ -39,3 +43,56 @@ def informe_matprima_faltante(request):
     materia_prima = MateriaPrima.objects.all()
     materia_prima_faltante = materia_prima.filter(cant_disponible__lt = F('cantidadMinima'))
     return render(request, 'informe/informe_matprima_faltante.html', {'materia_prima_faltante': materia_prima_faltante})
+
+def informe_productos_mas_vendidos(request):
+    items = Item.objects.all()
+
+    if request.method == 'POST':
+        fecha_inicio = request.POST.get('fec_inicio')
+        fecha_fin = request.POST.get('fec_fin')
+
+        if fecha_inicio and fecha_fin:
+            fecha_inicio = datetime.strptime(fecha_inicio, "%Y-%m-%d")
+            fecha_fin = datetime.strptime(fecha_fin, "%Y-%m-%d")
+            items = items.filter(
+                venta__fecha_venta__range=(fecha_inicio, fecha_fin)
+            )
+
+    # Agrupar por producto y sumar cantidades vendidas, luego ordenar y limitar a 10
+    productos_vendidos = (
+        items.values('producto__nombre')
+        .annotate(total_vendido=Count('venta', distinct=True))  # Cuenta las ventas únicas de cada producto
+        .order_by('-total_vendido')[:10]
+    )
+
+    '''
+    CONSULTA EN SQL
+    
+    SELECT producto.nombre AS producto__nombre, COUNT(DISTINCT item.venta_id) AS total_vendido
+    FROM item
+    INNER JOIN producto ON item.producto_id = producto.id
+    GROUP BY producto.nombre
+    ORDER BY total_vendido DESC
+    LIMIT 10;
+    '''
+
+    return render(request, 'informe/informe_productos_mas_vendidos.html', {'productos_vendidos': productos_vendidos})
+
+def informe_empleados_mas_ventas(request):
+    ventas = Venta.objects.all()
+
+    if request.method == 'POST':
+        fecha_inicio = request.POST.get('fec_inicio')
+        fecha_fin = request.POST.get('fec_fin')
+
+        if fecha_inicio and fecha_fin:
+            fecha_inicio = datetime.strptime(fecha_inicio, "%Y-%m-%d")
+            fecha_fin = datetime.strptime(fecha_fin, "%Y-%m-%d")
+            ventas = ventas.filter(fecha_venta__range=(fecha_inicio, fecha_fin))
+
+    empleados_mas_ventas = (
+        ventas.values('empleado__nombre_completo')
+        .annotate(total_ventas=Count('id'))
+        .order_by('-total_ventas')
+    )
+    return render(request, 'informe/informe_empleados_mas_ventas.html', {'empleados_mas_ventas': empleados_mas_ventas})
